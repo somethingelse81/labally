0cde6605aad6c657b129024f506e100b90324b44
Sanjay <sanjay.bagtharia@gmail.com>
2020-01-16 18:38:16 +0000

    Session Handling with Higher-Order Components

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,8 +1,5 @@
-import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-} from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navigation from '../Navigation';
 import LandingPage from '../Landing';
 import SignUpPage from '../SignUp';
@@ -12,47 +9,23 @@ import HomePage from '../Home';
 import AccountPage from '../Account';
 import AdminPage from '../Admin';
 import * as ROUTES from '../../constants/routes';
-import { withFirebase } from '../Firebase';
-
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      authUser: null,
-    };
-  }
-
-  componentDidMount() {
-    this.listener = this.props.firebase.auth.onAuthStateChanged(
-      authUser => {
-      authUser
-        ? this.setState({ authUser })
-        : this.setState({ authUser: null });
-    },
-    );
-  }
-  componentWillUnmount() {
-    this.listener();
-  }
-
-  render() {
-    return (
+import { withAuthentication } from '../Session';
+const App = () => (
   <Router>
     <div>
-      <Navigation authUser={this.state.authUser} />
+      <Navigation />
       <hr />
       <Route exact path={ROUTES.LANDING} component={LandingPage} />
       <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
       <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-      <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+      <Route
+        path={ROUTES.PASSWORD_FORGET}
+        component={PasswordForgetPage}
+      />
       <Route path={ROUTES.HOME} component={HomePage} />
       <Route path={ROUTES.ACCOUNT} component={AccountPage} />
       <Route path={ROUTES.ADMIN} component={AdminPage} />
     </div>
   </Router>
-    );
-  }
-}
-export default withFirebase(App);
+);
+export default withAuthentication(App);
